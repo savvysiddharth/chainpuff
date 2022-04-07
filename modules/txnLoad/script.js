e244efda7ff62204773c6bab0966316a72f0c8bb
Siddharth Maurya <savvysiddharth@gmail.com>
2022-04-08 02:53:57 +0530

    added latency graph and other basic metrics

--- modules/txnLoad/script.js
+++ modules/txnLoad/script.js
@@ -46,27 +46,20 @@ async function sendTransactions(txnOptions, batchSize, interTxnDelay) {
     const txnProm = w3.eth.sendTransaction(txnOptions).on('transactionHash', function(hash){
       // console.log(hash);
       let now = new Date();
+      // let now = Date.now();
       txnHashesTimes.push([hash, now]);
     });
-    await delay(interTxnDelay);
+    if(interTxnDelay > 0) await delay(interTxnDelay);
     if(i == batchSize-1) lastTxnProm = txnProm;
   }
   await delay(1000); // a hacky way to make sure all txn hash are stored in txnHashes
   return [txnHashesTimes, lastTxnProm];
 }
 
-function initialize() {
-  // puts default transaction payload
-  const txnInput = document.querySelector("#txnInput");
-  txnInput.innerHTML = defaultTransaction;
-
-  // adds event listener for button
-  const genBtn = document.querySelector("#genTxnLoad");
-  genBtn.addEventListener("click", genLoadHandler);
-}
-
 async function genLoadHandler() {
   console.log('Processing...');
+  myChart.destroy();
+  myChart = getNewChart();
   const numTxns = document.querySelector("#numTxns").value;
   const numBatches = document.querySelector("#numBatches").value;
   const txnDelay = document.querySelector("#txnDelay").value;
@@ -81,10 +74,11 @@ async function genLoadHandler() {
   }
 
   w3 = new Web3(nodeAddr);
-  const proms = [];
-  for(let i=0; i<numBatches; i++) {
-    // proms[i] = sendTransactions(txnOptions, numTxns, txnDelay);
-    // sleep(batchDelay);
+  let sumLatency = 0;
+  let totalTxns = 0;
+  let totalGasUsed = 0;
+  let totalBlocks = 0;
+  for(let i=0; i<numBatches; i++) { // 'i' IS CURRENT BATCH NUMBER IN THIS LOOP!!
     const txnSentReturn = await sendTransactions(txnOptions, numTxns, txnDelay);
     const hashTimes = txnSentReturn[0];
     const lastTxnProm = txnSentReturn[1];
@@ -100,34 +94,44 @@ async function genLoadHandler() {
     console.log(rcpts);
 
     const blockProms = [];
-    let totalGasUsed = 0;
-    for(let i=0; i<numTxns; i++) {
-      const blockNumber = rcpts[i].blockNumber;
+    const blockNums = [];
+    for(let j=0; j<numTxns; j++) {
+      const blockNumber = rcpts[j].blockNumber;
+      blockNums.push(blockNumber);
       const prom = w3.eth.getBlock(blockNumber);
       blockProms.push(prom);
-      totalGasUsed += rcpts[i].gasUsed;
+      totalGasUsed += rcpts[j].gasUsed;
     }
+    const uniqueBlockNums = Array.from(new Set(blockNums));
+    uniqueBlockNums.sort();
+    console.log(uniqueBlockNums);
+    totalBlocks += uniqueBlockNums.length;
     
-    const blockTimes = await Promise.all(blockProms); // actually grabs entire blocks
-    for(let i=0; i<numTxns; i++) {
-      blockTimes[i] = new Date(blockTimes[i].timestamp * 1000); // we don't need to store entire block, so replace block data with just its timestamp
+    // Calculating Latencies
+    let blocks = await Promise.all(blockProms); // actually grabs entire blocks
+    const blockTimes = [];
+    
+    for(let j=0; j<numTxns; j++) { // we don't need to store entire block, so replace block data with just its timestamp
+      blockTimes[j] = new Date(blocks[j].timestamp * 1000);
     }
-    // console.log(blockTimes);
+    blocks = []; // just deleting full blocks from memory
 
     const allLatencies = [];
-    for(let i=0; i<numTxns; i++) {
-      const timeGenerated = hashTimes[i][1];
-      const timeInBlock = blockTimes[i];
-      let latency = timeInBlock - timeGenerated; //milliseconds interval
+    for(let j=0; j<numTxns; j++) {
+      const timeGenerated = hashTimes[j][1];
+      const timeInBlock = blockTimes[j];
+      let latency = (timeInBlock - timeGenerated); //milliseconds interval
       allLatencies.push(latency);
       console.log(latency, "ms");
+      const txnNum = i*numTxns + j;
+      addData(myChart, txnNum, latency);
     }
-
-    let sum = 0;
-    for(let i=0; i<allLatencies.length; i++) {
-      sum += allLatencies[i];
+    
+    for(let j=0; j<allLatencies.length; j++) {
+      sumLatency += allLatencies[j];
     }
-    let avgLatency = sum/allLatencies.length;
+    totalTxns += allLatencies.length;
+    let avgLatency = sumLatency/totalTxns;
     console.log('Avg Latency:',avgLatency,"ms");
 
     const latencyText = document.querySelector("#avglatency");
@@ -136,41 +140,118 @@ async function genLoadHandler() {
     const totalGasText = document.querySelector("#totalgas");
     totalGasText.innerHTML = totalGasUsed + " Gas";
 
+    //Calculating Throughput
+    const uniqueBlockProms = [];
+    for(let j=0; j<uniqueBlockNums.length; j++) {
+      const prom = w3.eth.getBlock(uniqueBlockNums[j]);
+      uniqueBlockProms.push(prom);
+    }
+    const uniqueBlocks = await Promise.all(uniqueBlockProms);
+    console.log('uniqblk', uniqueBlocks);
+
+    const firstTxnTime = hashTimes[0][1];
+    const firstBlockTime = new Date(uniqueBlocks[0].timestamp * 1000);
+    let totalTime = firstBlockTime - firstTxnTime; // initialize with first txn latency
+    let totalTxns_inThisBatch = uniqueBlocks[0].transactions.length; // initialize
+    for(let j=1; j<uniqueBlocks.length; j++) {
+      const currblock = uniqueBlocks[j];
+      const prevblock = uniqueBlocks[j-1];
+      const currblock_time = new Date(currblock.timestamp * 1000);
+      const prevblock_time = new Date(prevblock.timestamp * 1000);
+      totalTime += (currblock_time - prevblock_time);
+      totalTxns_inThisBatch += currblock.transactions.length;
+    }
+    totalTime /= 1000;
+    console.log('totalTxns: ', totalTxns_inThisBatch);
+    console.log('totalTime: ',totalTime);
+    let throughput = totalTxns_inThisBatch/totalTime;
+
+    const throughputText = document.querySelector("#throughput");
+    throughputText.innerHTML = roundUp(throughput, 2, 3) + " Txn/sec";
+
+    console.log('totalGas: ', totalGasUsed);
+    console.log('totalBlocks: ',totalBlocks);
+    let avgGas = totalGasUsed / totalBlocks;
+    const gasPerBlockText = document.querySelector("#avggas");
+    gasPerBlockText.innerHTML = roundUp(avgGas, 2, 3) + " Gas/block";
+
     await delay(batchDelay);
   }
-
-  // const allPromises = [];
-  // const allLatencies = [];
-  // for(let i=0; i<numBatches; i++) {
-  //   for (let j=0; j<numTxns; j++) {
-  //     // console.log(proms[i][1]);
-  //     allPromises.push(proms[i][j][0]);
-  //     proms[i][j][0]
-  //     .then(() => {
-  //       const date2 = new Date();
-  //       const date1 = proms[i][j][1];
-  //       let diff = date2 - date1; //milliseconds interval
-  //       allLatencies.push(diff);
-  //       // console.log("Latency-"+i+','+j+": "+ diff + " ms");
-  //     });
-  //   } 
-  // }
-
-  // Promise.all(allPromises).then(() => {
-  //   console.log("i'm done!");
-  //   let sum = 0;
-  //   for(let i=0; i<allLatencies.length; i++) {
-  //     sum += allLatencies[i];
-  //   }
-  //   let avgLatency = sum/allLatencies.length;
-  //   console.log('Avg Latency:',avgLatency,"ms");
-  // });
 }
 
-function calculateThroughput(nodeAddr) {
-  w3 = new Web3(nodeAddr);
-  
+// src : https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary
+function roundUp(value, minimumFractionDigits, maximumFractionDigits) {
+  const formattedValue = value.toLocaleString('en', {
+    useGrouping: false,
+    minimumFractionDigits,
+    maximumFractionDigits
+  })
+  return Number(formattedValue)
+}
 
+function initialize() {
+  // puts default transaction payload
+  const txnInput = document.querySelector("#txnInput");
+  txnInput.innerHTML = defaultTransaction;
+
+  // adds event listener for button
+  const genBtn = document.querySelector("#genTxnLoad");
+  genBtn.addEventListener("click", genLoadHandler);
 }
 
 initialize();
+
+const ctx = document.getElementById('myChart');
+
+function addData(chart, label, data) {
+  chart.data.labels.push(label);
+  chart.data.datasets.forEach((dataset) => {
+      dataset.data.push(data);
+  });
+  chart.update();
+}
+
+function getNewChart() {
+  const newChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'latency (ms)',
+            data: [],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1,
+            pointRadius: 0
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: false,
+                color: "white"
+            },
+            x: {
+              color: "white"
+            }
+        }
+    }
+  });
+  return newChart;
+}
+
+let myChart = getNewChart();
